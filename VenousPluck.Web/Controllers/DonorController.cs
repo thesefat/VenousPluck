81bc4755c6cd0906522ddeb2f5bcf57d933ba59e
sefatanam <sefatanam@gmail.com>
2019-12-04 15:49:58 +0600

    Web-UI scratch design complete

---
+++ VenousPluck.Web/Controllers/DonorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VenousPluck.Web.Controllers
+{
+    public class DonorController : Controller
+    {
+        // GET: Donor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult ListOfDonors()
+        {
+            return View();
+        }
+    }
+}
